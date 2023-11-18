61902c61ade245bfd355e5104fffae36b0341dfc
24snehalb <snehalbadhe72@gmail.com>
2023-11-18 22:34:35 +0530

    remove the link field for block and school in session web form

--- appln_teacherbot_assam/mod_teacherbot_assam/web_form/session_tracking_tool/session_tracking_tool.py
+++ appln_teacherbot_assam/mod_teacherbot_assam/web_form/session_tracking_tool/session_tracking_tool.py
@@ -8,6 +8,7 @@ def get_context(context):
 def get_block(district_name):
     dd = frappe.db.sql(
         f"""select name from `tabMBTC_AS_Master_Block` where district = '{district_name}'""", as_dict=1)
+    print(dd)
     return '\n' + '\n'.join(data_item['name'] for data_item in dd)
 
 @frappe.whitelist(allow_guest=True)
